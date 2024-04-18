160e6fd49e98f552438dadf91b2449fb524bff88
ShehabOsama <shehab.osama@squadio.com>
2024-04-18 14:52:43 +0200

    update android test

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -22,7 +22,7 @@ public class BaseTest {
 
         if (PropertiesReader.getValue("Platform").equalsIgnoreCase("Android")) {
             MutableCapabilities capabilities = new UiAutomator2Options();
-            capabilities.setCapability("app","bs://fa395e54be9343e7e4de17921194a91c37ed3513");
+            capabilities.setCapability("app","bs://4f460a8dcf4e324616b5c3561f4a29dd76e8629b");
             capabilities.setCapability("platformName","android");
             capabilities.setCapability("deviceName","Samsung Galaxy S22 Ultra");
             driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
